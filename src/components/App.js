51600208e9d1cbf7c49d63522e70dd674795bd24
ulia <sivonenkoyulia@gmail.con>
2020-11-27 06:48:25 +0200

    task-2

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 
 import Profile from './Profile'
-// import Statistics from './Statistics'
+import Statistics from "./Statistics";
 // import FriendList from './FriendList'
 // import Transaction from './Transactions'
 
 import userData from "../data/user.json";
-// import statsData from '../data/statistical.json'
+import statsData from "../data/statistical.json";
 // import friends from '../data/friends.json'
 // import transactions from '../data/transactions.json'
 
 const App = () => {
   return (
-    
-           <>
-            <Profile name={userData.name}
-                tag={userData.tag}
-                location={userData.loation}
-                avatar={userData.avatar}
-                stats={userData.stats}
-              />
-             </>
-        
-    );
+    <>
+      <Profile
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.loation}
+        avatar={userData.avatar}
+        stats={userData.stats}
+      />
+      <Statistics items={statsData}/>
+    </>
+  );
 };
 
 export default App;
